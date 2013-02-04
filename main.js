011c27c557ce879b76e829f9dde468483e886c41
Mark Woodall <mawoodall@gmail.com>
2013-02-03 20:23:25 -0600

    serial.js Integration working

--- main.js
+++ main.js
@@ -4,84 +4,112 @@
 
 
 var connectionId = -1;
-
-function onOpen() {
-  if (serial_lib.isConnected) {
-    setStatus('Could not open');
-    return;
-  }
-  setStatus('Connected');
-}
+var bitrates = [300, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 115200];
 
 function setStatus(status) {
-  document.getElementById('status').innerText = status;
+    document.getElementById('status').innerText = status;
 }
 
-function buildPortPicker(ports) {
-  var eligiblePorts = ports.filter(function(port) {
-    return !port.match(/[Bb]luetooth/);
-  });
-
-  var portPicker = document.getElementById('port-picker');
-  eligiblePorts.forEach(function(port) {
-    var portOption = document.createElement('option');
-    portOption.value = portOption.innerText = port;
-    portPicker.appendChild(portOption);
-  });
-
-  portPicker.onchange = function() {
-    if (connectionId != -1) {
-      serial_lib.closeSerial(serial_lib.onClose);
-      return;
+function onOpen() {
+    if (!serial_lib.isConnected()) {
+        setStatus('Could not open');
+        return;
     }
-    openSelectedPort();
-  };
+    setStatus('Connected');
 }
 
 function openSelectedPort() {
-  var portPicker = document.getElementById('port-picker');
-  var selectedPort = portPicker.options[portPicker.selectedIndex].value;
-  serial_lib.openSerial(selectedPort, onOpen);
+    var portPicker = document.getElementById('port-picker');
+    var selectedPort = portPicker.options[portPicker.selectedIndex].value;
+    var bitratePicker = document.getElementById('bitrate-picker');
+    var selectedBitrate = bitratePicker.options[bitratePicker.selectedIndex].value;
+    serial_lib.setBitrate(parseInt(selectedBitrate,10));
+    serial_lib.openSerial(selectedPort, onOpen);
 }
 
-var writeSerial=function(writeString) {
+function buildPortPicker(ports) {
+    var eligiblePorts = ports.filter(function (port) {
+        return !port.match(/[Bb]luetooth/);
+    });
+
+    var portPicker = document.getElementById('port-picker');
+    eligiblePorts.forEach(function (port) {
+        var portOption = document.createElement('option');
+        portOption.value = portOption.innerText = port;
+        portPicker.appendChild(portOption);
+    });
+
+    portPicker.onchange = function () {
+        if (connectionId !== -1) {
+            serial_lib.closeSerial(serial_lib.onClose);
+            return;
+        }
+        openSelectedPort();
+    };
+}
+
+function buildBitratePicker(bitrates) {
+
+    var brPicker = document.getElementById('bitrate-picker');
+    bitrates.forEach(function (br) {
+        var brOption = document.createElement('option');
+        brOption.value = brOption.innerText = br;
+        brPicker.appendChild(brOption);
+    });
+    
+
+    brPicker.value = serial_lib.getBitrate();
+    
+    brPicker.onchange = function () {
+        if (connectionId !== -1) {
+            serial_lib.closeSerial(serial_lib.onClose);
+            return;
+        }
+        openSelectedPort();
+    };
+}
+    
+
+var writeSerial = function (writeString) {
     if (!serial_lib.isConnected()) {
-      return;
+        return;
     }
     if (!writeString) {
-      logError("Nothing to write");
-      return;
+        console.log("Nothing to write");
+        return;
     }
-    if (writeString.charAt(writeString.length-1)!=='\n') {
-      writeString+="\n"; 
+    if (writeString.charAt(writeString.length - 1) !== '\n') {
+        writeString += "\n";
     }
-    serial_lib.writeSerial(writeString); 
-}
+    serial_lib.writeSerial(writeString);
+};
 
-var onRead=function(readData) {
-    if (readData.indexOf("log:")>=0) {
-      return;
+var onRead = function (readData) {
+    if (readData.indexOf("log:") >= 0) {
+        console.log("log:");
+        return;
     }
-    
+
     var output = document.getElementById('output');
     output.innerHTML += readData + '<br/>';
 };
 
 
-onload = function() {
-  serial_lib.getPorts(function(ports) {
-    buildPortPicker(ports);
-    openSelectedPort();
-    
-    var input = document.getElementById('input');
-    var sendButton = document.getElementById('send');
-    
-    sendButton.addEventListener('click', function() {
-        writeSerial(input);
-        serial_lib.startListening(onRead);
-    })
-    //setInterval(function() { writeSerial("data"); }
+onload = function () {
+    serial_lib.getPorts(function (ports) {
+        buildPortPicker(ports);
+        buildBitratePicker(bitrates);
+        openSelectedPort();
 
-    
-  });
+
+        var sendButton = document.getElementById('send');
+
+        sendButton.addEventListener('click', function () {
+            var input = document.getElementById('input').value;
+            writeSerial(input);
+            setInterval(function () {
+                serial_lib.startListening(onRead);
+            }, 200);
+        });
+    });
 };
